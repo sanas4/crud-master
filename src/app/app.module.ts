9f240aff5e05a8c2b5b11f3e6b810eaff7abc55f
sanas4 <63055290+sanas4@users.noreply.github.com>
2020-04-03 17:33:31 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AvatarDialogComponent} from './avatar-dialog/avatar-dialog.component';
+import {EditUserComponent} from './edit-user/edit-user.component';
+import {HomeComponent} from './home/home.component';
+import {NewUserComponent} from './new-user/new-user.component';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule} from '@angular/material';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {FirebaseService} from './services/firebase.service';
+import {EditUserResolver} from './edit-user/edit-user.resolver';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarDialogComponent,
+    EditUserComponent,
+    HomeComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService, EditUserResolver],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDiWIjcOvhD8UCV09-f-n3WlnzH4tPwYCI',
+    authDomain: 'fir-master-1e89f.firebaseapp.com',
+    databaseURL: 'https://fir-master-1e89f.firebaseio.com',
+    projectId: 'fir-master-1e89f',
+    storageBucket: 'fir-master-1e89f.appspot.com',
+    messagingSenderId: '968315300773',
+    appId: '1:968315300773:web:8052c52f4f4c7c2212d64c',
+    measurementId: 'G-9XC4NLKNP1'
+  }
 };
 
 /*
